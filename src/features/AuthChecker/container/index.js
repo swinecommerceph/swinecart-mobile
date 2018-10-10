7a5890a994266a88e6f362aad98368072fb5bf77
giooooo <gbperalta1@up.edu.ph>
2018-10-10 15:57:53 +0800

    Fix cant find variable symbol issue

--- src/features/AuthChecker/container/index.js
+++ src/features/AuthChecker/container/index.js
@@ -1,3 +1,69 @@
-export {
-  default
-} from '../components';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  AsyncStorage
+} from 'react-native';
+import {
+  Container,
+  Content
+} from 'native-base'
+import Spinner from 'react-native-spinkit';
+
+import {
+  observer, inject
+} from 'mobx-react';
+
+import { Navigation } from '../../../services/';
+
+@inject('UserStore', 'CommonStore')
+@observer
+class AuthChecker extends Component {
+
+  componentDidMount() {
+    this.checkToken();
+  }
+
+  checkToken = async () => {
+    const token = await AsyncStorage.getItem('token');
+    const { 
+      CommonStore, UserStore
+    } = this.props;
+    if(token) {
+      await CommonStore.setToken(token);
+      await UserStore.getUser();
+      setTimeout(() => {
+        Navigation.navigate(UserStore.userRole);
+      }, 500);
+    }
+    else {
+      setTimeout(() => {
+        Navigation.navigate('Public');
+      }, 300);
+    }
+  }
+
+  render() {
+    return (
+      <Container style={{ flex: 1 }}>
+        <Content contentContainerStyle={[styles.container]}>
+          <Spinner
+            type='ThreeBounce'
+            color='#ffffff'
+            size={100}
+          />
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00af66',
+  }
+});
+
+export default AuthChecker;
