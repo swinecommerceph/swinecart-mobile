ccf2df748fdacc2d8525e22a16edf25505c49646
giooooo <gbperalta1@up.edu.ph>
2020-02-05 16:52:09 +0800

    refactor: new project structure

---
+++ src/components/basic/Input/index.js
@@ -0,0 +1,86 @@
+import React, { useMemo, useCallback, memo, useState } from 'react';
+import { withStyles, Input as UKInput } from '@ui-kitten/components';
+import isEqual from 'react-fast-compare';
+
+import { sizes, colors } from 'constants/theme';
+
+import { computeLineHeight } from 'utils';
+
+import Icon from '../Icon';
+
+function Input(props) {
+
+  const [ isVisible, setIsVisible ] = useState(true);
+
+  const {
+    name, values, touched, errors, label, placeholder,
+    onChange, isPassword,
+    themedStyle, ...otherProps
+  } = props;
+
+  
+  const hasError = useMemo(() => !!(errors[name]), [ errors[name] ]);
+
+  const onChangeText = useCallback(value => {
+    onChange(name, value);
+  }, [ values[name] ]);
+
+  const renderIcon = () => (
+    <Icon 
+      name={ isVisible ? 'eye' : 'eye-off' } 
+      size={26} 
+      color={colors.gray5}
+      marginHorizontal={0.5}
+    />
+  );
+
+  const onIconPress = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <UKInput
+      size='small'
+      placeholder={placeholder}
+      label={label}
+      status={hasError ? 'danger' : 'primary'}
+      caption={hasError ? errors[name] : null}
+      value={`${values[name] || ''}`}
+      style={themedStyle.input}
+      textStyle={themedStyle.inputText}
+      labelStyle={themedStyle.labelText}
+      captionTextStyle={themedStyle.captionText}
+      onChangeText={onChangeText}
+      icon={isPassword ? renderIcon : null}
+      onIconPress={isPassword ? onIconPress : null}
+      secureTextEntry={isPassword ? isVisible : false}
+      {...otherProps}
+    />
+  );
+
+}
+
+export default withStyles(memo(Input, isEqual), () => ({
+  input: {
+    marginBottom: sizes.margin
+  },
+  inputText: {
+    fontFamily: 'OpenSans-SemiBold',
+    fontWeight: 'normal',
+    fontSize: 16,
+    lineHeight: computeLineHeight(16)
+  },
+  labelText: {
+    fontFamily: 'OpenSans-SemiBold',
+    fontWeight: 'normal',
+    color: colors.primary,
+    fontSize: 14,
+    lineHeight: computeLineHeight(14)
+  },
+  captionText: {
+    fontFamily: 'OpenSans-SemiBold',
+    fontWeight: 'normal',
+    fontSize: 14,
+    lineHeight: computeLineHeight(14)
+  }
+}));
--- src/components/basic/index.js
+++ src/components/basic/index.js
@@ -1,1 +1,3 @@
 export { default as Block } from './Block';
+export { default as Text } from './Text';
+export { default as Input } from './Input';
