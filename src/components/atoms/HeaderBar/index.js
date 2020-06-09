0ad380da30e0de30771af869908dca0bbe12063d
giooooo <gbperalta1@up.edu.ph>
2020-06-09 08:31:05 +0800

    refactor: change Headerbar title alignment to start

--- src/components/atoms/HeaderBar/index.js
+++ src/components/atoms/HeaderBar/index.js
@@ -23,7 +23,7 @@ function HeaderBar(props) {
         style={headerBarStyle}
         title={title}
         titleStyle={themedStyle.titleStyle}
-        alignment='left'
+        alignment='start'
         leftControl={leftControl}
         rightControls={rightControls}
       />
