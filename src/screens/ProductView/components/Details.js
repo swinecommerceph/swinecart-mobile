93b5ddb983c590c47bae19b3a129a4413ef9c54e
giooooo <gbperalta1@up.edu.ph>
2020-12-01 12:27:22 +0800

    add add to cart button in product view

--- src/screens/ProductView/components/Details.js
+++ src/screens/ProductView/components/Details.js
@@ -1,6 +1,10 @@
 import React, { Fragment, memo, useEffect } from 'react';
 import { useStoreState, useStoreActions } from 'easy-peasy';
+
+import { ModalService } from 'services';
+
 import { ContainerView } from 'molecules';
+import { Button, Block, LoadingOverlay } from 'atoms';
 
 import ProductInfo from './ProductInfo';
 import SwineInfo from './SwineInfo';
@@ -9,6 +13,8 @@ import Images from './Images';
 
 function Details() {
 
+  const isAddingItem = useStoreState(state => state.cart.isAddingItem);
+  const accountType = useStoreState(state => state.user.accountType);
   const data = useStoreState(state => state.productView.data);
 
   const {
@@ -20,13 +26,31 @@ function Details() {
     images,
   } = data;
 
+
+  const onPressAdd = () => {
+    ModalService.showModal('AddToCart', { ...productInfo });
+  };
+
   return (
-    <ContainerView paddingBottom={2} backgroundColor='white1'>
-      <Images data={images} type={productInfo.type} />
-      <ProductInfo data={productInfo} breeder={breeder} farm={farm} />
-      <SwineInfo data={swineInfo} type={productInfo.type} />
-      <OtherDetails data={otherDetails} />
-    </ContainerView>
+    <Fragment>
+      <LoadingOverlay show={isAddingItem} />
+      <ContainerView paddingBottom={2} backgroundColor='white1'>
+        <Images data={images} type={productInfo.type} />
+        <ProductInfo data={productInfo} breeder={breeder} farm={farm} />
+        <SwineInfo data={swineInfo} type={productInfo.type} />
+        <OtherDetails data={otherDetails} />
+      </ContainerView>
+      {
+        accountType === 'Customer'
+        ?
+          <Block padding={0.5}>
+            <Button size='small' onPress={onPressAdd}>
+              Add to Cart
+            </Button>
+          </Block>
+        : []
+      }
+    </Fragment>
   );
 }
 
